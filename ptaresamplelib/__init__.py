1b1bf09a0325fdc1dfedb682fc31f73a3c4480ed
Rutger van Haasteren <vhaasteren@gmail.com>
2015-03-30 12:40:52 -0700

    Added the actual resample library

---
+++ ptaresamplelib/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+
+from ptaresamplelib import *
+
+__version__ = 2015.03
+
+def test():
+    # Run some tests here
+    print("{0} tests have passed".format(0))
